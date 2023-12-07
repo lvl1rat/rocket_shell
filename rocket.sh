c46533b917e1259024292a9f62aaa72f2c95f953
Bruno Franchini <brfranch9@gmail.com>
2023-12-07 01:29:40 -0300

    addsleep

--- rocket.sh
+++ rocket.sh
@@ -3,7 +3,7 @@
 clear
 tput sc
 tput civis
-for i in {1..100}; do tput cup 1 $i && echo '---]>====>>>'; done
+for i in {1..100}; do tput cup 1 $i && echo '---]>====>>>' && sleep .025; done
 #tput rc; tput el
 tput setaf 1
 tput cup 1 $i && echo '<<<<BOOM>>>>'
@@ -11,3 +11,4 @@ tput sgr0
 i=0
 for i in {1..100}; do tput cup 1 $i && echo ' '; done
 tput cnorm
+
